d7475aaf72675f109cf82e9452dec5be687691a2
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-07-13 13:41:33 +0900

    Hello world Example

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.status(200).send("Hi! My name is Ryo");
+});
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
