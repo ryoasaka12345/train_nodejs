9527efb9c47133dbdbb7bb4189a7af99b8222106
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-07-13 16:57:31 +0900

    Create user model

---
+++ entity/User.ts
@@ -0,0 +1,20 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    profession: string;
+}
