940239ed7a852470f16be267f167ddcda9688345
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-07-13 16:21:24 +0900

    installation

--- index.js
+++ index.js
@@ -24,29 +24,50 @@ app.get("/", (req, res) => {
     res.status(200).send("Hi! My name is Ryo");
 });
 
-app.get('/listUsers', function(req, res) {
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
+app.get('/listUsers', function (req, res) {
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
     });
 })
 
 app.post('/addUser', function (req, res) {
     // Prepare output in JSON format
     user4 = {
-        name:req.body.name,
-        password:req.body.password,
-        profession:req.body.profession,
-        id:req.body.id
+        name: req.body.name,
+        password: req.body.password,
+        profession: req.body.profession,
+        id: req.body.id
     };
     console.log(user4);
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-        data = JSON.parse( data );
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse(data);
         data["user4"] = user4;
-        console.log( data );
-        res.end( JSON.stringify(data));
+        console.log(data);
+        res.end(JSON.stringify(data));
     });
 })
+
+app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        var users = JSON.parse(data);
+        var user = users["user" + req.params.id];
+        console.log(user);
+        res.end(JSON.stringify(user));
+    });
+})
+
+app.delete('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        delete data["user" + req.params.id];
+
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+});
 
 /**
  * Server Activation
