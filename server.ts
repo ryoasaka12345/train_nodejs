57867cde9c48769c883a7f3bd35546de12b1915b
浅香 諒 <ryo.asaka@g.pascalia.co.jp>
2021-07-14 14:46:07 +0900

    step1 generate a token

--- server.ts
+++ server.ts
@@ -6,6 +6,8 @@ import * as bodyParser from "body-parser";
 // import { AppRoutes } from "./routes";
 import { getManager } from "typeorm";
 import { User } from "./entity/User";
+import * as jwt from "jsonwebtoken";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 const ormOptions: any = {
 	type: "mysql",
@@ -27,13 +29,39 @@ createConnection(ormOptions)
 		// create express app
 		const app = express();
 		app.use(bodyParser.json());
+		const TOKEN_SECRET =
+			"5f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4";
+		app.use(bodyParser.json());
+		function generateAccessToken(email) {
+			return jwt.sign(email, TOKEN_SECRET, { expiresIn: "1800s" });
+		}
 
 		// Routes Definitions
+		app.post("/login", async (req, res) => {
+			const userRepository = getManager().getRepository(User);
+
+			// load a user by email and password
+			const user = await userRepository.find({
+				where: {
+					email: req.body.email,
+					password: req.body.password,
+				},
+			});
+			// return 403 if user is not wxisting
+			if (!user || user.length === 0) {
+				res.sendStatus(403);
+			}
+
+			// generate token and return to client
+			const token = generateAccessToken({ email: req.body.email });
+			res.json(token);
+		});
+
 		app.get("/", (req, res) => {
 			res.status(200).send("Hi!. My name is Ryo");
 		});
 
-		app.get('/listUsers', async function (req, res) {
+		app.get("/listUsers", async function (req, res) {
 			// get a user repository to perform operations with user
 			const userRepository = getManager().getRepository(User);
 
@@ -43,13 +71,13 @@ createConnection(ormOptions)
 			res.send(users);
 		});
 
-		app.post('/addUser', async function (req, res) {
+		app.post("/addUser", async function (req, res) {
 			// Prepare output in JSON format
 			const user = {
 				email: req.body.email,
 				name: req.body.name,
 				password: req.body.password,
-				profession: req.body.profession
+				profession: req.body.profession,
 			};
 			console.log(user);
 
@@ -61,7 +89,35 @@ createConnection(ormOptions)
 			// return loaded users
 			res.send(users);
 		});
-		
+
+		app.get("/:id", async function (req, res) {
+			// First read existing users.
+			// get a user repository to perform operations with user
+			const userRepository = getManager().getRepository(User);
+
+			// load a user by a given user id
+			const user = await userRepository.findOne(+req.params.id);
+
+			// if user was not found return 404 to the client
+			if (!user) {
+				res.status(404);
+				res.end();
+				return;
+			}
+
+			// return loaded user
+			res.send(user);
+		});
+
+		app.delete("/:id", async function (req, res) {
+			// First read existing users.
+			// get a user repository to perform operations with user
+			const userRepository = getManager().getRepository(User);
+
+			userRepository.delete(req.params.id);
+			res.end();
+			return;
+		});
 
 		// run app
 		app.listen(3000);
